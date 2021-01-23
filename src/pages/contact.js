0a014a079e48a0b6bb3ba7ba527e6b8d7f4170ab
semdinsp <scott.sproule@ficonab.com>
2021-01-23 17:30:11 +0800

    better message in the contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,7 +17,7 @@ const ContactPage = ({
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
+          <p>Let me you grow and manage your business accurately, &rarr;</p>
         </div>
         <div>
         <form className="form-container" name="mtgcontact" data-netlify="true"  data-netlify-honeypot="bot-field" action="/success"  method="POST">
