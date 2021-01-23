b8f44bc09c8f6dd93c198001ab9e646865dde6d4
semdinsp <scott.sproule@ficonab.com>
2021-01-23 17:09:40 +0800

    update for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,22 +20,26 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
+        <form className="form-container" name="contact" data-netlify="true"  data-netlify-honeypot="bot-field" action="/success"  method="POST">
             <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="mtgcontact" />
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
+              <label htmlFor="Name">Name</label>
+              <input type="text" name="Name" id="cName"/>
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
+              <label htmlFor="Sender">Email</label>
+              <input type="email" name="Sender" id="cSender"/>
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <label htmlFor="Subject">Subject</label>
+              <input type="text" name="Subject" id="cSubject"/>
+            </div>
+            <div>
+              <label htmlFor="Message">Message</label>
+              <textarea name="Message" id="cMessage"></textarea>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
               <input type="submit" className="button -primary" style={{marginRight: 0}} />
