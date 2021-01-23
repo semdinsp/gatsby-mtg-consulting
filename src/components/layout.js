643289d9ba0f33395f273fbfe85bd36d046a8064
semdinsp <scott.sproule@ficonab.com>
2021-01-23 16:47:29 +0800

    fix for typo

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} MTG Consulting LLC &bull; Crafted with <span role="img" aria-label="love">❤️</span> </a></p>
+        <p>&copy; {new Date().getFullYear()} MTG Consulting LLC &bull; Crafted with <span role="img" aria-label="love">❤️</span> </p>
       </footer>
     </div>
   )
